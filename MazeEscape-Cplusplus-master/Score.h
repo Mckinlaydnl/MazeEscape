a715ffd9dc281b5e3f5a032ea31e3f7f482b2b57
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2018-11-07 13:59:04 +0000

    Added score

--- MazeEscape-Cplusplus-master/Score.h
+++ MazeEscape-Cplusplus-master/Score.h
@@ -2,9 +2,18 @@
 
 // Project Includes
 #include "TextObject.h"
+#include "Player.h"
 
 class Score : public TextObject
 {
 public:
 	Score();
+	//Overriding methods
+	virtual void Update(sf::Time _frameTime);
+
+	// Public Score Methods
+	void SetPlayer(Player* _player);
+
+private:
+	Player * m_player;
 };
